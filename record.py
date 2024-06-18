8c2d9bd716e6bba22135ec76ec7ea9dcc48df0c3
Yiling Cao <yiling.cao@gmail.com>
2024-06-18 12:30:23 +0800

    add filename by ip address.

--- record.py
+++ record.py
@@ -10,6 +10,23 @@ def get_seconds_until_next_hour():
     next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
     return (next_hour - now).seconds
 
+def get_ip_from_rtsp_url(rtsp_url):
+    # Find the position of '@' and ':' after 'rtsp://'
+    at_pos = rtsp_url.find('@')
+    if at_pos == -1:
+        return None  # Invalid RTSP URL
+
+    colon_pos = rtsp_url.find(':', at_pos)
+    if colon_pos == -1:
+        return None  # Invalid RTSP URL
+
+    # Extract the IP address part
+    ip_address = rtsp_url[at_pos + 1:colon_pos]
+
+    # Replace dots with hyphens
+    modified_ip_address = ip_address.replace('.', '-')
+    return modified_ip_address
+
 def record_stream(rtsp_url, duration=3600):
     # Define the output file name with timestamp
     now_utc = datetime.now(pytz.utc)
@@ -17,7 +34,7 @@ def record_stream(rtsp_url, duration=3600):
     now_shanghai = now_utc.astimezone(pytz.timezone('Asia/Shanghai'))
 
     # Output file name with timestamp in Shanghai/Asia time
-    output_file = "hyL1-"+now_shanghai.strftime("%Y-%m-%d_%H-%M") + ".mkv"
+    output_file = get_ip_from_rtsp_url(rtsp_url)+"_"+now_shanghai.strftime("%Y-%m-%d_%H-%M") + ".mkv"
 
     print(duration, output_file)
     time.sleep(0.1)
@@ -29,8 +46,8 @@ def record_stream(rtsp_url, duration=3600):
         '-i', rtsp_url,
         '-t', str(duration),
         '-c:v', 'copy',  # Copy the video stream as is
-        #'-c:a', 'copy',  # Copy the audio stream as is (if present)
-        '-c:a', 'libopus', '-b:a', '24k', # Copy the audio stream as is (if present)
+        '-c:a', 'copy',  # Copy the audio stream as is (if present)
+        #'-c:a', 'libopus', '-b:a', '16k', # Copy the audio stream as is (if present)
         '-metadata', 'title=',  # Set the metadata title
         output_file
     ]
