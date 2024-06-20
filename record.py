394ad56f8dc34f2a996470b41821b1900eac4018
Yiling Cao <yiling.cao@gmail.com>
2024-06-20 12:17:55 +0000

    add subdir and some sleep interval for new file

--- record.py
+++ record.py
@@ -4,11 +4,12 @@ import pytz
 import json
 from datetime import datetime, timedelta
 
+base_dir = "/var/www/nvr/"
 
 def get_seconds_until_next_hour():
     now = datetime.now()
     next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
-    return (next_hour - now).seconds
+    return (next_hour - now).seconds + 5
 
 def get_ip_from_rtsp_url(rtsp_url):
     # Find the position of '@' and ':' after 'rtsp://'
@@ -33,15 +34,19 @@ def record_stream(rtsp_url, duration=3600):
     # Convert UTC time to Shanghai/Asia time
     now_shanghai = now_utc.astimezone(pytz.timezone('Asia/Shanghai'))
 
+    sub_dir = now_utc.astimezone(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d") + "/"
+    #mkdir focefully
+    subprocess.run(["mkdir", "-p", base_dir+sub_dir])
+
     # Output file name with timestamp in Shanghai/Asia time
-    output_file = get_ip_from_rtsp_url(rtsp_url)+"_"+now_shanghai.strftime("%Y-%m-%d_%H-%M") + ".mkv"
+    output_file = base_dir + sub_dir +get_ip_from_rtsp_url(rtsp_url)+"_"+now_shanghai.strftime("%Y-%m-%d_%H-%M") + ".mkv"
 
     print(duration, output_file)
     time.sleep(0.1)
 
     # FFmpeg command to capture the stream without re-encoding
     ffmpeg_command = [
-        'ffmpeg',
+        'ffmpeg', '-y',
         '-rtsp_transport', 'tcp',
         '-i', rtsp_url,
         '-t', str(duration),
@@ -51,7 +56,7 @@ def record_stream(rtsp_url, duration=3600):
         '-metadata', 'title=',  # Set the metadata title
         output_file
     ]
-
+    print(ffmpeg_command)
     # Execute the FFmpeg command
     subprocess.run(ffmpeg_command)
 
